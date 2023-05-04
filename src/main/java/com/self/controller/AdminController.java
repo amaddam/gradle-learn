370ad0fa55f706950a2f8ec29d56d36b364d1f70
“fanchao” <fanchao.shen@capgemini.com>
2023-05-05 01:09:52 +0800

    [Fanchao] #N/A feat: 升级依赖版本

--- src/main/java/com/self/controller/AdminController.java
+++ src/main/java/com/self/controller/AdminController.java
@@ -8,18 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/admin")
+//@Slf4j
 public class AdminController {
-
+Logger logger = Logger.getLogger("AdminController");
     @Autowired
     private AdminService adminService;
 
+//    http://localhost:8080/admin/list
     @RequestMapping("/list")
     @ResponseBody
     public List<Admin> getAdminList() {
-        System.out.println("dada");
+        logger.log(Level.INFO,"查找所有管理员");
         return adminService.getAdminList();
     }
 
