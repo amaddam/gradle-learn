3c8f10b5a80ea94f2cdb8a9433d704e85ac9672e
ameduam <lrns1-edu@outlook.com>
2024-01-11 16:02:31 +0800

    Fanchao: feat: Start at file operate

---
+++ gradle03-subproject03/src/main/java/com/self/Main.java
@@ -0,0 +1,8 @@
+package com.self;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+//        "D:\\JAVA\\jdk\\graalvm-ce-java17-22.3.0";
+    }
+}
---
+++ src/main/java/com/self/MultipleMain.java
@@ -0,0 +1,10 @@
+package com.self;
+
+import java.util.List;
+
+public class MultipleMain {
+    public static void main(String[] args) {
+        System.out.println("主启动类");
+//        List.of(args).forEach(System.out::println);
+    }
+}
