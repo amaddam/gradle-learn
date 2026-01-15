26e845ca023b1d0a85f87526ddfccf5e324e7fc0
ameduam <lrns1-edu@outlook.com>
2026-01-15 09:32:04 +0800

    gradle项目重新整合

---
+++ src/main/java/com/self/test/AppTest.java
@@ -0,0 +1,16 @@
+package com.self.test;
+
+//import org.junit.jupiter.api.Test;
+
+/**
+ * @author ：lrns1
+ * @date ：Created in 2023年05月05日
+ * @description ：
+ * @version: 1.0
+ */
+public class AppTest {
+//    @Test
+    public void test() {
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/com/self/test2/AppTest2.java
@@ -0,0 +1,17 @@
+package com.self.test2;
+
+
+import org.junit.Test;
+
+/**
+ * @author ：lrns1
+ * @date ：Created in 2023年05月05日
+ * @description ：
+ * @version: 1.0
+ */
+public class AppTest2 {
+    @Test
+    public void test() {
+        System.out.println("hello world2");
+    }
+}
